821cd4fd2d126398462a4bd7be3d7fc64d707146
Kohei.Shigeta <blkms1996@gmail.com>
2022-08-20 14:42:42 +0900

    fix port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const exporess = require("express");
 require("dotenv").config();
 
 const app = exporess();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 // console.log(PORT);
 // console.log(`access port ${PORT}`);
 app.use(exporess.json());
@@ -14,7 +14,7 @@ app.use("/api/v1/setting", settingRoute)
 
 const start = async () => {
     try{
-        app.listen(process.env.PORT || PORT, console.log("サーバーが起動しました"));
+        app.listen(PORT, console.log("サーバーが起動しました"));
     }catch(err){
         console.log(err)
     }
